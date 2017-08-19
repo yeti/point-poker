a6689a837bf25180cb698a8de228a038909d6364
Nico <nicolas.schomberg@gmail.com>
2017-08-19 09:50:19 -0700

    Hot load working

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,8 @@
 // Import React and React-dom.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-
-// Import the components.
-import Layout from './components/layout';
-import Home from './components/home';
-import Auth from './components/auth';
-import About from './components/about';
-import Contact from './components/contact';
-import Room from './components/room';
-import Join from './components/join';
+import { AppContainer } from 'react-hot-loader'
+import Routes from './Routes'
 
 // Import service worker
 import Worker from './serviceWorker';
@@ -19,21 +11,22 @@ import Worker from './serviceWorker';
 const root = document.querySelector('main');
 
 // Append the DummyComponent to the root element.
-ReactDOM.render(
-  (
-    <Router history={browserHistory}>
-      <Route component={Layout}>
-        <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/contact" component={Contact}/>
-        <Route path="/join" component={Join}/>
-        <Route path="/join/:room" component={Auth}/>
-        <Route path="/join/:room/:user" component={Room}/>
-        <Route path="/*" component={Home}/>
-      </Route>
-    </Router>
-  ), root);
+const render = Component => ReactDOM.render(
+  <AppContainer>
+    <Component />
+  </AppContainer>,
+  root
+);
 
 // Init the service worker
 const serviceWorker = new Worker();
 serviceWorker.register();
+
+render(Routes);
+
+if (module.hot) {
+  module.hot.accept('./Routes', () => {
+    const NextRoot = require('./Routes').default;
+    render(NextRoot);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client',
     './src/js/index'
   ],
@@ -11,9 +12,7 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  devtool: 'inline-source-map',
   module: {
     loaders: [{
       test: /\.js$/,
@@ -21,5 +20,15 @@ module.exports = {
       include: [path.join(__dirname, 'src'), /node_modules/],
       exclude: /node_modules/
     }]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+  ],
 };
